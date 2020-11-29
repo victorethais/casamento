b17eb457d399f5b6d8ce792747c1bc09ddbe5532
Victor Marques <victormatheus92@gmail.com>
2020-11-29 17:02:50 -0300

    Updates

---
+++ precache-manifest.8accb266d1d77fc324fd02177b5a0dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2ce238bafd76f88414cd9cc6db64eb",
+    "url": "/casamento/index.html"
+  },
+  {
+    "revision": "50661b6a60f10fec7e47",
+    "url": "/casamento/static/js/2.735fb6cc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/casamento/static/js/2.735fb6cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b38ed4e04924993db9",
+    "url": "/casamento/static/js/main.32fd59a2.chunk.js"
+  },
+  {
+    "revision": "e3316ee0feb3d3ceb6b6",
+    "url": "/casamento/static/js/runtime-main.b5558441.js"
+  },
+  {
+    "revision": "47dc246964a71a09274d145c260ff545",
+    "url": "/casamento/static/media/flor.47dc2469.png"
+  }
+]);
